305a52d3bd346dec9969066e955c8a3a112d48f0
udayahindu <67618511+udayahindu@users.noreply.github.com>
2022-04-20 11:41:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 
 import streamlit
 streamlit.title('My Mom\'s New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3& blueberry oat meal')
+streamlit.text('kale,spinach & rocket smoothie')
+streamlit.text('hard boiled free-range egg')
+
