29d6c391cceeb9f2690aaaca247f56e5167b52e9
udayahindu <67618511+udayahindu@users.noreply.github.com>
2022-04-23 16:49:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+from urllib.error import URLError
 streamlit.title('My Mom\'s New Healthy Dinner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -24,6 +25,7 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choic
 
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+streamlit.stop()
 import snowflake.connector
 my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=my_cnx.cursor()
