6461c055300bd4090b02c4ca0d37437b83951fb2
udayahindu <67618511+udayahindu@users.noreply.github.com>
2022-04-23 15:59:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,11 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choic
 
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
-
+import snowflake.connector
+my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur=my_cnx.cursor()
+my_cur.execute("select CURRENT_USER(),CURRENT_ACCOUNT(),CURRENT_REGION()")
+my_data_row=my_cur.fetchone()
+streamlit.text("hello from snowflake:")
+streamlit.text(my_data_row)
 
